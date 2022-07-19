af9ca387607a0e44dd394af7e538d1d11a78de19
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-19 14:52:13 +0700

    Initial Commit

---
+++ EmojiBook-WidgetKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiBook-WidgetKit
+//
+//  Created by 7Peaks (Nine) on 19/7/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiBook-WidgetKit/EmojiBook_WidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiBook_WidgetKitApp.swift
+//  EmojiBook-WidgetKit
+//
+//  Created by 7Peaks (Nine) on 19/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct EmojiBook_WidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
