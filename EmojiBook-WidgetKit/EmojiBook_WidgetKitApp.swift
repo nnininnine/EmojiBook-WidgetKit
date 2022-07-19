7e04dcedcc5fce87a336bcb56e0ea9244444fed5
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-19 14:55:15 +0700

    feat: add HomeView

--- EmojiBook-WidgetKit/EmojiBook_WidgetKitApp.swift
+++ EmojiBook-WidgetKit/EmojiBook_WidgetKitApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct EmojiBook_WidgetKitApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      HomeView()
     }
+  }
 }
---
+++ EmojiBook-WidgetKit/Screens/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  EmojiBook-WidgetKit
+//
+//  Created by 7Peaks (Nine) on 19/7/2565 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
