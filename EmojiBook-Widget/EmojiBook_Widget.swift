7614210587932bec14305ff6af1be603acd712be
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-20 13:21:32 +0700

    feat(widget): add EmojiWidgetView in EmojiBook_WidgetEntryView, EmojiBook_Widget

--- EmojiBook-Widget/EmojiBook_Widget.swift
+++ EmojiBook-Widget/EmojiBook_Widget.swift
@@ -5,65 +5,69 @@
 //  Created by Nuttapon Buaban on 20/7/2565 BE.
 //
 
-import WidgetKit
-import SwiftUI
 import Intents
+import SwiftUI
+import WidgetKit
 
-struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+struct Provider: TimelineProvider {
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date(), emoji: EmojiProvider.shared.rand())
+  }
+
+  func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+    let entry = SimpleEntry(date: Date(), emoji: EmojiProvider.shared.rand())
+    completion(entry)
+  }
+
+  func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+    var entries: [SimpleEntry] = []
+
+    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+    let currentDate = Date()
+    for hourOffset in 0 ..< 5 {
+      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+      let entry = SimpleEntry(date: entryDate, emoji: EmojiProvider.shared.rand())
+      entries.append(entry)
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
-        completion(entry)
-    }
-
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+    let timeline = Timeline(entries: entries, policy: .atEnd)
+    completion(timeline)
+  }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationIntent
+  let date: Date
+  let emoji: Emoji
 }
 
-struct EmojiBook_WidgetEntryView : View {
-    var entry: Provider.Entry
+struct EmojiBook_WidgetEntryView: View {
+  var entry: Provider.Entry
 
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
+  var body: some View {
+    EmojiWidgetView(emoji: entry.emoji)
+  }
 }
 
 @main
 struct EmojiBook_Widget: Widget {
-    let kind: String = "EmojiBook_Widget"
+  let kind: String = "EmojiBook_Widget"
 
-    var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            EmojiBook_WidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+  var body: some WidgetConfiguration {
+    StaticConfiguration(
+      kind: kind,
+      provider: Provider()
+    ) { entry in
+      EmojiBook_WidgetEntryView(entry: entry)
     }
+    .configurationDisplayName("Random Emoji")
+    .description("Display a widget with an emoji that is updated randomly.")
+    .supportedFamilies([.systemSmall])
+  }
 }
 
 struct EmojiBook_Widget_Previews: PreviewProvider {
-    static var previews: some View {
-        EmojiBook_WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
+  static var previews: some View {
+    EmojiBook_WidgetEntryView(entry: SimpleEntry(date: Date(), emoji: EmojiProvider.shared.rand()))
+      .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
 }
