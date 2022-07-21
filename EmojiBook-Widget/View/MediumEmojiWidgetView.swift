4634af0589544192d307d0bc19209df80995b199
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-21 10:29:58 +0700

    feat(widget): implement MediumEmojiWidgetView

--- EmojiBook-Widget/View/MediumEmojiWidgetView.swift
+++ EmojiBook-Widget/View/MediumEmojiWidgetView.swift
@@ -6,15 +6,39 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct MediumEmojiWidgetView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  // MARK: Properties
+
+  let emoji: Emoji
+
+  // MARK: Body
+
+  var body: some View {
+    ZStack {
+      Color.indigo
+
+      HStack(alignment: .center) {
+        Text(emoji.emoji)
+          .font(.system(size: 56))
+          .padding(8)
+        VStack(alignment: .leading) {
+          Text(emoji.name)
+            .font(.headline)
+          Text(emoji.description)
+            .multilineTextAlignment(.leading)
+        }
+        .foregroundColor(.white)
+      }
+      .padding()
     }
+  }
 }
 
 struct MediumEmojiWidgetView_Previews: PreviewProvider {
-    static var previews: some View {
-        MediumEmojiWidgetView()
-    }
+  static var previews: some View {
+    MediumEmojiWidgetView(emoji: EmojiProvider.shared.rand())
+      .previewContext(WidgetPreviewContext(family: .systemMedium))
+  }
 }
