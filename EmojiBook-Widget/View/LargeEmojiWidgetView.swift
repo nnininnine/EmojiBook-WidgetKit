20e3e029f722c66ffb3a7be849fd7e1d2ed79c97
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-21 10:46:51 +0700

    feat(widget): implement LargeEmojiWidgetView

--- EmojiBook-Widget/View/LargeEmojiWidgetView.swift
+++ EmojiBook-Widget/View/LargeEmojiWidgetView.swift
@@ -6,15 +6,39 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct LargeEmojiWidgetView: View {
+  // MARK: Properties
+
+  let emoji: Emoji
+
+  // MARK: Body
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      Color.indigo
+
+      VStack {
+        Text(emoji.name)
+          .font(.title)
+          .bold()
+        Text(emoji.emoji)
+          .font(.system(size: 56))
+          .padding(4)
+        Text(emoji.description)
+          .font(.title3)
+      } //: VStack
+      .padding()
+      .foregroundColor(.white)
+      .multilineTextAlignment(.center)
+    } //: ZStack
   }
 }
 
 struct LargeEmojiWidgetView_Previews: PreviewProvider {
   static var previews: some View {
-    LargeEmojiWidgetView()
+    LargeEmojiWidgetView(emoji: EmojiProvider.shared.rand())
+      .previewContext(WidgetPreviewContext(family: .systemLarge))
   }
 }
