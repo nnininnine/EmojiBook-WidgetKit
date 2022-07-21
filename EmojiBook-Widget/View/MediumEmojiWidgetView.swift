efe7e78f14b982d005329c875c89628edb037df7
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-21 10:21:19 +0700

    feat(widget): create MediumEmojiWidgetView

---
+++ EmojiBook-Widget/View/MediumEmojiWidgetView.swift
@@ -0,0 +1,20 @@
+//
+//  MediumEmojiWidgetView.swift
+//  EmojiBook-WidgetExtension
+//
+//  Created by 7Peaks (Nine) on 21/7/2565 BE.
+//
+
+import SwiftUI
+
+struct MediumEmojiWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MediumEmojiWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediumEmojiWidgetView()
+    }
+}
