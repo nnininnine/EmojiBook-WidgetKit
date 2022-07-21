3bf369ba35cd36cd8fa5747358e7d2bf94355216
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-21 10:31:04 +0700

    feat(widget): create SmallEmojiWidgetView

---
+++ EmojiBook-Widget/View/SmallEmojiWidgetView.swift
@@ -0,0 +1,21 @@
+//
+//  SmallEmojiWidgetView.swift
+//  EmojiBook-WidgetExtension
+//
+//  Created by 7Peaks (Nine) on 21/7/2565 BE.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct SmallEmojiWidgetView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct SmallEmojiWidgetView_Previews: PreviewProvider {
+  static var previews: some View {
+    SmallEmojiWidgetView()
+  }
+}
