d1dde197d64fe954164f1bd30a8c15fa4ec03c50
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-21 10:39:17 +0700

    feat(widget): create LargeEmojiWidgetView

---
+++ EmojiBook-Widget/View/LargeEmojiWidgetView.swift
@@ -0,0 +1,20 @@
+//
+//  LargeEmojiWidgetView.swift
+//  EmojiBook-WidgetExtension
+//
+//  Created by 7Peaks (Nine) on 21/7/2565 BE.
+//
+
+import SwiftUI
+
+struct LargeEmojiWidgetView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct LargeEmojiWidgetView_Previews: PreviewProvider {
+  static var previews: some View {
+    LargeEmojiWidgetView()
+  }
+}
